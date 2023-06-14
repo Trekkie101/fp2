a817c3069b2f82540315c5a93296f79ca294e8ac
Graeme Spence <graemespence@midnightmacness.local>
2023-06-14 23:09:54 +0100

    RSS proof of concept + RSS library

--- index.php
+++ index.php
@@ -1,7 +1,38 @@
 <?php
 
+$starttime = microtime(true); // Top of page
 
-echo "Hello World";
+
+require_once 'Feed.php';
+
+$rss = Feed::loadRss('http://www.simplemachines.org/community/index.php?action=.xml;sa=news;limit=5;type=rss2');
+	
+echo 'Site: ', $rss->title;
+
+echo '<br ><br />';
+
+$c = 0;
+
+foreach ($rss->item as $item) {
+
+	$c++;
+		
+	echo 'Title: ', $item->title;
+	echo ' - ', $item->url;
+	echo ' - ';
+	echo sha1($item->title);
+	echo '<br />';
+}
+
+print $c;
+
+
+	echo '<br /><br /><br /><br />';
+
+$endtime = microtime(true); // Bottom of page
+
+printf("Load time: %f seconds", $endtime - $starttime );
+
 
 
 ?>
