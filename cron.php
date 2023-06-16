30a2a29f482558866cf524477a9dd5a6450c2f3d
Graeme Spence <graemespence@midnightmacness.local>
2023-06-16 01:46:05 +0100

    Todo //

--- cron.php
+++ cron.php
@@ -2,6 +2,14 @@
 
 $today = date("Ymd"); 
 
+// Todo
+
+// Check last run was more than 1 hour ago
+// Find a way to log fails
+// Mark success
+// Set actual cron to run
+
+
 
 ini_set("error_log", "log/".$today."-php_errors.txt");
 
